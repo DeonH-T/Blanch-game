a91bb2766c048e974ae306cf41081a55b31fb52b
DeonH-T <89952463+DeonH-T@users.noreply.github.com>
2021-09-02 20:16:31 +1200

    Movement updates
    Added
    -jumping
    -falling
    changed the way that movement works

--- Blanch/Assets/Scripts/PlayerMovement.cs
+++ Blanch/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,9 @@ public class PlayerMovement : MonoBehaviour
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
+
+    private enum MovementState { idle, running, jumping, falling }
+    
     // Start is called before the first frame update
     private void Start()
     {
@@ -36,20 +39,33 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimationState()
     {
+        MovementState state;
+
        if (dirX > 0f)
        {
-           anim.SetBool("Running", true);
+           state = MovementState.running; 
            sprite.flipX = false;
        }
        else if (dirX < 0f)
        {
-           anim.SetBool("Running", true);
+           state = MovementState.running;
            sprite.flipX = true;
        }
        else 
        {
-           anim.SetBool("Running", false);
+           state = MovementState.idle;
        }
+
+       if (rb.velocity.y > .1f)
+       {
+           state = MovementState.jumping;
+       }
+       else if (rb.velocity.y < -1f)
+       {
+           state = MovementState.falling;
+       }
+
+        anim.SetInteger("state",(int)state);
     }    
     
 }
